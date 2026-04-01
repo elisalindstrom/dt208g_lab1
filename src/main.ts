2f7edd5654561f5ede2b41cafa647beaafb22030
elisalindstrom <elisalindstroem@gmail.com>
2026-04-01 19:25:30 +0200

    Uppdaterat funktion som skriver ut  kurser. Lagt till funktion för borttagning av kurs från lista.

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,19 @@ import './style.css'
 interface Course {
   code: string;
   name: string;
-  progression: string;
+  progression: "A" | "B" | "C"; // Literal types
   syllabus: string;
 }
+printCourse();
 
 // Formulär
 const courseForm = document.querySelector("#course-form") as HTMLFormElement;
+const errorMessage = document.querySelector("#error-message") as HTMLParagraphElement;
 
-function displayCourse(course: Course): void {
-  const courseList = document.querySelector("#course-list");
-  courseList?.classList.remove("hidden");
-
-  if (courseList) {
-    const liEl = document.createElement("li");
-    liEl.innerHTML = `${course.code} | ${course.name} | ${course.progression} | ${course.syllabus}`;
-    
-    courseList.appendChild(liEl);
-  }
-}
-
+// Lägg till kurs vid submit
 courseForm.addEventListener("submit", (event) => {
   event.preventDefault();
+  let savedCourses: Course[] = JSON.parse(localStorage.getItem("courses") || "[]");
 
   const codeInput = document.querySelector("#course-code") as HTMLInputElement;
   const nameInput = document.querySelector("#course-name") as HTMLInputElement;
@@ -34,9 +26,66 @@ courseForm.addEventListener("submit", (event) => {
   const newCourse: Course = {
     code: codeInput.value,
     name: nameInput.value,
-    progression: progressionSelection.value,
+    progression: progressionSelection.value as "A" | "B" | "C",
     syllabus: syllabusInput.value,
   };
 
-  displayCourse(newCourse);
+  // Kontroll om kurskoden redan finns sparad i localStorage
+  let courseExists: boolean = savedCourses.some(course => course.code === newCourse.code);
+
+  // Om kurskoden inte finns sparad
+  if (courseExists === false) {
+    errorMessage.classList.add("hidden");
+    courseForm.reset();
+
+    savedCourses.push(newCourse);
+    localStorage.setItem("courses", JSON.stringify(savedCourses));
+
+    printCourse();
+  } else {
+    errorMessage.classList.remove("hidden");
+    errorMessage.textContent = "Kursen är redan sparad, försök med en annan kurskod";
+  }
 })
+
+// Skriv ut kurs i kurslista
+function printCourse(): void {
+  const courseList = document.querySelector("#course-list") as HTMLUListElement;
+  courseList.innerHTML = "";
+
+  let savedCourses: Course[] = JSON.parse(localStorage.getItem("courses") || "[]");
+
+  if (savedCourses.length === 0) {
+    courseList.classList.add("hidden");
+    return;
+  }
+  courseList.classList.remove("hidden");
+
+  savedCourses.forEach(course => {
+    const liEl = document.createElement("li");
+    liEl.innerHTML = `<div class="list-head">
+                      <span class="strong">${course.code}</span>
+                      <button class="remove-btn">Ta bort</button>
+                      </div>
+                      <div class="list-row">
+                      <span>${course.name}</span>
+                      <span>Progression ${course.progression}</span>
+                      <a href="${course.syllabus}" target="_blank">Kursplan</a>
+                      </div>`;
+
+    courseList.appendChild(liEl);
+
+    const deleteBtn = liEl.querySelector(".remove-btn") as HTMLButtonElement;
+    deleteBtn.addEventListener("click", () => {
+      deleteCourse(course.code);
+    })
+  })
+}
+
+// Ta bort kurs från kurslista
+function deleteCourse(code: string): void {
+  let savedCourses: Course[] = JSON.parse(localStorage.getItem("courses") || "[]");
+  savedCourses = savedCourses.filter(course => course.code !== code);
+  localStorage.setItem("courses", JSON.stringify(savedCourses));
+  printCourse();
+}
